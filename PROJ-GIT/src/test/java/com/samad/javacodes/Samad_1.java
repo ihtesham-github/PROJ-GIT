87b20e05b5e8597c0af9a08ca655ea49ae19ff01
samad <samad_chy@yahoo.com>
2022-11-02 13:32:54 -0500

    Added new class

---
+++ PROJ-GIT/src/test/java/com/samad/javacodes/Samad_1.java
@@ -0,0 +1,16 @@
+package com.samad.javacodes;
+
+public class Samad_1 {
+
+	public static void main(String[] args) {
+		
+		//String reverse
+		
+		String abc = "agsfjgasfjgA";
+		String ABC = abc.toUpperCase();
+		char ch[] = ABC.toCharArray();
+		for (int i=ABC.length()-1; i>0; i--) 
+			System.out.print(i);
+	}
+
+}
