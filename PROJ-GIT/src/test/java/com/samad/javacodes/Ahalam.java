efa0296d05b38a995d9670c7d3a096778ef4dfc0
ahalam <ahalam105@gmail.com>
2022-11-02 11:59:54 -0500

    Add a Class(class_name-Ahalam)

---
+++ PROJ-GIT/src/test/java/com/samad/javacodes/Ahalam.java
@@ -0,0 +1,22 @@
+package com.samad.javacodes;
+
+public class Ahalam {
+
+	public static void main(String[] args) {
+
+		int  z= 6;
+		
+		do {
+			System.out.println("Good morning");
+			z--;
+		} while (z>10);
+		
+		int  z1= -6;
+		
+		do {
+			System.out.println("Good night");
+			z1++;
+		} while (z1<10);
+	}
+
+}
