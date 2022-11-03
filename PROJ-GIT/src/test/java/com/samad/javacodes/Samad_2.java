fb4921ade53b6e47444b032348066556deed1234
fahim <fahim@aklaptop>
2022-11-03 00:01:47 -0500

    new add

--- PROJ-GIT/src/test/java/com/samad/javacodes/Samad_2.java
+++ PROJ-GIT/src/test/java/com/samad/javacodes/Samad_2.java
@@ -3,7 +3,11 @@ package com.samad.javacodes;
 public class Samad_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//ONE: Replace characters in string
+		
+				String str = "aabbccddeeff";
+				String newStr= str.replace("a", "X");
+				System.out.println(newStr);
 
 	}
 
