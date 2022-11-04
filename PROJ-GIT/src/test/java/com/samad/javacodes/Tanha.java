d957478a01460330b5c7a8be009fb64aabf578cc
Tanha <tanhatuly@gmail.com>
2022-11-04 12:09:42 -0400

    Added code

---
+++ PROJ-GIT/src/test/java/com/samad/javacodes/Tanha.java
@@ -0,0 +1,13 @@
+package com.samad.javacodes;
+
+public class Tanha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//print even numbers from 0 to 20
+        for(int i=0; i<=20;i+=2) {
+        System.out.println(i);	
+       }
+	}
+
+}
