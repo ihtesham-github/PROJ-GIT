7dc7deed9425b810387c8142d2f85814d06dfab5
KHADIJA <khadija.begum.bd@gmail.com>
2022-11-14 12:10:31 -0600

    I add some java code

--- PROJ-GIT/src/test/java/com/samad/javacodes/Khadija.java
+++ PROJ-GIT/src/test/java/com/samad/javacodes/Khadija.java
@@ -2,18 +2,25 @@ package com.samad.javacodes;
 
 public class Khadija {
 
-	//public static void main(String[] args) {
-		// Adding natural numbers upto given digit
-		
-		public void addNumbers(int a) {
-			int sum =0;
-			for(int i=0;i<=a; i++) {
-				sum = sum+i;
-			}
-			
-				System.out.println("Sum od upto "+a+": + sum");
-			}
+	// public static void main(String[] args) {
+	// Adding natural numbers upto given digit
+
+	public void addNumbers(int a) {
+		int sum = 0;
+		for (int i = 0; i <= a; i++) {
+			sum = sum + i;
 		}
-	
 
+		System.out.println("Sum od upto " + a + ": + sum");
 
+		int numbers = 23456789, reverse = 0;
+		while (numbers != 0) {
+			int numbers1 = 23456789 % 10;
+			int remainder = 0;
+			reverse = reverse * 10 + remainder;
+			numbers1 = numbers1 / 10;
+			System.out.println("The reverse of the given number is:" + reverse);
+
+		}
+	}
+}
--- PROJ-GIT/src/test/java/com/samad/javacodes/Khadija1.java
+++ PROJ-GIT/src/test/java/com/samad/javacodes/Khadija1.java
@@ -9,9 +9,35 @@ public class Khadija1 {
 
 	public static void main(String[] args) {
 		List<String> colors = new ArrayList<>(Arrays.asList("RED", "BLUE", "BLACK"));
-		 
-        Collections.reverse(colors);
-        System.out.println(colors);
+
+		Collections.reverse(colors);
+		System.out.println(colors);
+
+		// List<String>fruits = new ArrayList<>(Array.asList("mango", "banana",
+		// "apple"));
+		// Collections.reverse(fruits);
+
+		// System.out.println(fruits);
+		List<String> color1 = new ArrayList<>(Arrays.asList("RED", "BLUE", "BLACK"));
+
+		Collections.reverse(colors);
+		System.out.println(colors);
+
+		int number = 987654, reverse = 0;
+		while (number != 0) {
+			int remainder = number % 10;
+			reverse = reverse * 10 + remainder;
+			number = number / 10;
+		}
+		System.out.println("The reverse of the given number is: " + reverse);
+
+//        int numbers = 23456789, reverse1 = 0;
+//        while(numbers != 0)
+//        {
+//        	int remainder = 23456789 % 10;
+//        	reverse1 = reverse1 * 10+ remainder;
+//        	number = number/10;
+//        	System.out.println("The reverse of the given number is:" + reverse1);
 	}
 
 }
