7a69a47d5789137f717a5fea4a78103fcaa27034
rashed <ahrashed410@gmail.com>
2022-11-18 07:23:32 -0800

    I add some code

--- PROJ-GIT/src/test/java/com/samad/javacodes/Rashed.java
+++ PROJ-GIT/src/test/java/com/samad/javacodes/Rashed.java
@@ -1,10 +1,21 @@
 package com.samad.javacodes;
 
+import java.util.Arrays;
+
 public class Rashed {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+    String [] shoe = {"adidas","nike","new balance","reebok","puma"};
+    System.out.println(shoe[0]);
+    System.out.println(shoe[1]);
+    System.out.println(shoe[2]);
+    System.out.println(shoe[3]);
+    System.out.println(shoe[4]);
+    shoe [3]= "under armour";
+    System.out.println(shoe[3]);
+    System.out.println(shoe.length);
+    System.out.println(Arrays.toString(shoe));
 	}
 
 }
